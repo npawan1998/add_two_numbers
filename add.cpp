684c7345881dc678162fa8f56e71e39e6a763416
npawan98 <44532194+npawan98@users.noreply.github.com>
2018-10-29 21:31:53 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(){
-	int a,b,sum;
+	int num1,num2,sum;
 
 	cout<<"enter two numbers:"<<endl; //prompt the user
-	cin>>a>>b; // takes the two values 
+	cin>>num1>>num1; // takes the two values 
 
-	sum=a+b; //operation is done in this variable 
+	sum=num1+num2; //operation is done in this variable 
 
 	cout<<<"result:"<<endl;
 	cout<<sum; // prints the sum
